647dd5b9ad67b977b05d6d0de6634cf95b868fbc
PackYokohama <packpommadee@gmail.com>
2020-01-28 16:42:55 +0700

    Lap999999

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,4 +1,18 @@
-//cout << "-------------------- SOTUS ---------------------";
-//cout << textline;
-//cout << "-------------------- SOTUS ---------------------";
-//Don't forget to change cout to output filestream
+#include <fstream>
+#include <string
+using namespace
+
+int main(){
+    ifstream source;
+    ofstream dest("cheerbook_coppyy.txt");
+    source.open("cheerbook.txt")
+
+
+dest << "-------------------- SOTUS ---------------------"<< "\n";
+string textline;
+while(getline(souurce,testline)){
+    dest << textline << "\n";
+}
+dest << "-------------------- SOTUS ---------------------";
+return 0;
+}
